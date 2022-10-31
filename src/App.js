3b90884b3e57da1b626b2d237b14090ebf6782a4
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-10-31 11:18:26 +0200

    add user name prompt and its functionality
    - create userName comp
    - consume store data in userName comp.
    - use conditional rendering to only view prompt on loading then hide on submit user name
    - add user name form and input
    - add local state to control user name input
    - add hndleChnage & handleSubmit methods to handle local state and store useData to the store
    - style user name prompt by adding bootstrap classes and style rules in index.css
    - import user name comp in App component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Provider } from "react-redux";
 import store from "./store/index";
 
+// components import
+import UserName from "./components/UserName";
+
 function App() {
   return (
     <Provider store={store}>
-      <div className="App"></div>
+      <div className="App">
+        <div className="container my-5">
+          <UserName />
+        </div>
+      </div>
     </Provider>
   );
 }
---
+++ src/components/UserName.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "../store/slices/userNameSlice";
+
+const UserName = () => {
+  // consume data from store
+  const state = useSelector((state) => state.userNameReducer);
+  // allow dispatching actions
+  const dispatch = useDispatch();
+  const { setUserName, hideUserNamePrompt } = actions;
+
+  // local component state
+  const [userName, setLocalUserName] = useState("");
+
+  // component methods
+  const handleNameChange = (e) => setLocalUserName(e.target.value);
+
+  const handleSubmit = (e) => {
+    // prevent default behavior of submitting then reload
+    e.preventDefault();
+
+    // store user name to store
+    dispatch(setUserName(userName));
+
+    // hide uer name prompt
+    dispatch(hideUserNamePrompt());
+  };
+
+  return (
+    <>
+      {state.showPrompt && (
+        <div className="prompt-container text-secondary">
+          <form
+            onSubmit={handleSubmit}
+            className="p-3 bg-white border rounded mx-3 text-start"
+          >
+            <h1 className="text-center mb-4">
+              Hi there, we'd like to know your name :)
+            </h1>
+            {/* user name input  */}
+            <div className="my-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Your Name:
+              </label>
+              <input
+                type="text"
+                className="form-control mb-4"
+                value={userName}
+                placeholder="John Doe"
+                onChange={handleNameChange}
+              />
+            </div>
+            {/* submit button  */}
+            <div className="text-end">
+              <button type="submit" className="btn btn-primary">
+                Go to quiz
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UserName;
