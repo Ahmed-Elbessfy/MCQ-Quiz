b542af950b922127a29b78e8660bfc231d4d2468
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2022-11-01 10:46:18 +0200

    add Result style
    - add success and fail or danger styles at index.css
    
    - use conditional rendering to only show result after submitting answers
    
    - adding success and fail message and gifs

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,43 @@
 import { useSelector } from "react-redux";
 
 const Result = () => {
-  const state = useSelector((state) => state);
-  return <>{state.scoreStore.showScore && <h1>{state.scoreStore.score}</h1>}</>;
+  // consume data from store
+  const state = useSelector((state) => state.scoreStore);
+
+  return (
+    <>
+      {state.showScore &&
+        (state.score > 0 ? (
+          // Success result > 0
+          <div className="text-success">
+            <h1 className="score score-success">
+              {state.score}
+              <br />
+            </h1>
+            <p className="h2 my-4">Yaaaaah! You did it! Let's celebrate</p>
+            {/* <img
+            src="https://www.spanishconsalsa.com/wp-content/uploads/friday-celebration-gif-jackie-chan-chris-tucker-car-dancing-rush-hour.gif"
+            alt=""
+          /> */}
+            <img
+              src="https://createifwriting.com/wp-content/uploads/2019/04/celebrate-gif.gif"
+              alt={"Yaaaaaah! you did it! your score is " + state.score}
+            />
+          </div>
+        ) : (
+          // Fail result <= 0
+          <div className="text-danger">
+            <h1 className="score score-danger">{state.score}</h1>
+            <p className="h2 my-3">You should be studying!</p>
+
+            <img
+              src="https://gifimage.net/wp-content/uploads/2017/07/angry-cat-gif-18.gif"
+              alt={"Your score is " + state.score + "! Go study"}
+            />
+          </div>
+        ))}
+    </>
+  );
 };
 
 export default Result;
